94793be569bf3f6f5d3eca5de547aed859ea05d3
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2014-05-18 23:00:42 +0900

    参考URLの追加

--- SimpleWeather/AppDelegate.h
+++ SimpleWeather/AppDelegate.h
@@ -5,6 +5,8 @@
 //  Created by ucuc on 2014/05/18.
 //  Copyright (c) 2014年 ucuc. All rights reserved.
 //
+//  iOS 7 Best Practices; A Weather App Case Study: Pt 1 - Ray Wenderlich
+//  http://www.raywenderlich.com/55384/ios-7-best-practices-part-1
 
 #import <UIKit/UIKit.h>
 
